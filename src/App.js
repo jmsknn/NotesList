91ef226e19015d282f691c92149503ede1e17997
James <jameswebguru512@gmail.com>
2020-09-24 17:18:08 -0400

    router integration and create main structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
+} from 'react-router-dom';
 
-function App() {
+import { Dashboard, NotePage } from './pages';
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/note" component={NotePage} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-}
-
-export default App;
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware, connectRouter } from 'connected-react-router';
 
 export const history = createBrowserHistory();
 
-const middleware = [routerMiddleware(history)];
+const middlewares = [routerMiddleware(history)];
 const rootReducer = combineReducers({
   router: connectRouter(history),
 });
 
-const store = configureStore({
-  reducer: rootReducer,
-  middleware,
-});
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewares),
+);
 
 export default store;
