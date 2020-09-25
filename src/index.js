4065ae7bac63ad580fccb2c95ada93fddc016646
James <jameswebguru512@gmail.com>
2020-09-24 23:05:35 -0500

    configured router & fixed error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import store, { history } from './store';
-import { App } from './App';
+import store from "./store";
+import { App } from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
+    <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
